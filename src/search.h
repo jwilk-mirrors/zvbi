cf1dafd1a95ec2f08abf46b23a46a7882cb8c887
Michael H. Schimek <mschimek@users.sourceforge.net>
2004-02-25 17:28:33 +0000

    vbi_search_next, vbi_search_next_va_list: Now takes Teletext page formatting options like vbi_format_vt_page(). Some doc and type fixes.s/vbi_search_new/vbi_search_ucs2_new. Added vbi_search_utf8_new() which takes search string in UTF-8 instead of UCS-2 encoding.
    git-svn-id: https://svn.code.sf.net/p/zapping/svn/branches/branch-0-3/vbi@2027 746cafe9-179e-40ba-aef4-f754c8c453d8

--- src/search.h
+++ src/search.h
@@ -21,7 +21,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: search.h,v 1.2 2002-07-16 00:11:36 mschimek Exp $ */
+/* $Id: search.h,v 1.2.2.1 2004-02-25 17:28:33 mschimek Exp $ */
 
 #ifndef SEARCH_H
 #define SEARCH_H
@@ -33,55 +33,105 @@ typedef struct vbi_decoder vbi_decoder;
 
 /* Public */
 
+#include <stdarg.h>
+
 /**
  * @ingroup Search
- * @brief Return codes of the vbi_search_next() function.
+ * Return codes of the vbi_search_next() function.
  */
 typedef enum {
 	/**
-	 * Pattern not found, @a pg is invalid. Another vbi_search_next()
-	 * will restart from the original starting point.
+	 * Some error occured, condition unclear. Call vbi_search_delete().
 	 */
 	VBI_SEARCH_ERROR = -3,
 	/**
-	 * The search has been canceled by the progress function.
-	 * @a pg points to the current page as in success case,
-	 * except for the highlighting. Another vbi_search_next()
-	 * continues from this page.
+	 * No pages in the cache, @a pg is invalid.
 	 */
 	VBI_SEARCH_CACHE_EMPTY,
 	/**
-	 * No pages in the cache, @a pg is invalid.
+	 * The search has been aborted by the progress callback function.
+	 * @a pg points to the current page as in success case, except
+	 * no text is highlighted. Another vbi_search_next() call
+	 * continues from this page.
 	 */
-	VBI_SEARCH_CANCELED,
+	VBI_SEARCH_ABORTED,
 	/**
-	 * Some error occured, condition unclear. Call vbi_search_delete().
+	 * Pattern not found, @a pg is invalid. Another vbi_search_next()
+	 * call restarts from the original starting point.
 	 */
 	VBI_SEARCH_NOT_FOUND = 0,
 	/**
-	 * Pattern found. @a pg points to the page ready for display with the pattern
-	 *   highlighted, @a pg->pgno etc.
+	 * Pattern found. @a pg points to the page ready for display with
+	 * the found pattern highlighted.
 	 */
 	VBI_SEARCH_SUCCESS
 } vbi_search_status;
 
 /**
  * @ingroup Search
- * @brief Opaque search context. 
+ * @brief Search context.
+ *
+ * The contents of this structure are private.
+ *
+ * Call vbi_search_new_ucs2() or vbi_search_new_utf8() to
+ * allocate a search context.
  */
 typedef struct vbi_search vbi_search;
+
+/**
+ * @ingroup Search
+ * @param search Search context passed to vbi_search_next().
+ * @param pg Current page being searched, formatted as requested
+ *   with vbi_search_next(). Do not modify or free this page, if
+ *   necessary you can call vbi_page_copy().
+ * @param user_data User data pointer passed to vbi_search_next().
+ *
+ * Search progress callback function.
+ *
+ * @returns:
+ * @c FALSE to abort the search, then vbi_search_next() will return
+ * @c VBI_SEARCH_ABORTED.
+ */
+typedef vbi_bool
+vbi_search_progress_cb		(vbi_search *		search,
+				 const vbi_page *	pg,
+				 void *			user_data);
 
 /**
  * @addtogroup Search
  * @{
  */
-extern vbi_search *	vbi_search_new(vbi_decoder *vbi,
-				       vbi_pgno pgno, vbi_subno subno,
-				       uint16_t *pattern,
-				       vbi_bool casefold, vbi_bool regexp,
-				       int (* progress)(vbi_page *pg));
-extern void		vbi_search_delete(vbi_search *search);
-extern vbi_search_status vbi_search_next(vbi_search *search, vbi_page **pg, int dir);
+extern vbi_search *
+vbi_search_new_ucs2		(vbi_decoder *		vbi,
+				 vbi_pgno		pgno,
+				 vbi_subno		subno,
+				 const uint16_t *	pattern,
+				 unsigned int		pattern_size,
+				 vbi_bool		casefold,
+				 vbi_bool		regexp,
+				 vbi_search_progress_cb *progress,
+				 void *			user_data);
+extern vbi_search *
+vbi_search_new_utf8		(vbi_decoder *		vbi,
+				 vbi_pgno		pgno,
+				 vbi_subno		subno,
+				 const char *		pattern,
+				 vbi_bool		casefold,
+				 vbi_bool		regexp,
+				 vbi_search_progress_cb *progress,
+				 void *			user_data);
+extern void
+vbi_search_delete		(vbi_search *		search);
+extern vbi_search_status
+vbi_search_next_va_list		(vbi_search *		search,
+				 vbi_page **		pg,
+				 int			dir,
+				 va_list		format_options);
+extern vbi_search_status
+vbi_search_next			(vbi_search *		search,
+				 vbi_page **		pg,
+				 int			dir,
+				 ...);
 /** @} */
 
 /* Private */
