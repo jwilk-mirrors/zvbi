f424da61ee850f679b1671328a7becb60f7bb570
Michael H. Schimek <mschimek@users.sourceforge.net>
2008-02-27 07:57:47 +0000

    Rewrote the GCC __attribute__ wrapper macros for compatibility with strict ISO C. Updated from branch 0.2. Changed the license to LGPLv2+.
    git-svn-id: https://svn.code.sf.net/p/zapping/svn/branches/branch-0-3/vbi@3991 746cafe9-179e-40ba-aef4-f754c8c453d8

--- src/dvb_demux.h
+++ src/dvb_demux.h
@@ -1,23 +1,25 @@
 /*
- *  libzvbi - DVB VBI demultiplexer
+ *  libzvbi -- DVB VBI demultiplexer
  *
  *  Copyright (C) 2004, 2007 Michael H. Schimek
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This library is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the 
+ *  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+ *  Boston, MA  02110-1301  USA.
  */
 
-/* $Id: dvb_demux.h,v 1.3.2.5 2008-02-25 21:00:55 mschimek Exp $ */
+/* $Id: dvb_demux.h,v 1.3.2.6 2008-02-27 07:57:47 mschimek Exp $ */
 
 #ifndef __ZVBI3_DVB_DEMUX_H__
 #define __ZVBI3_DVB_DEMUX_H__
