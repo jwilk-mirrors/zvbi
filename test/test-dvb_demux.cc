547d03ab7ea55c162811229de011e88bf671d1b2
Michael H. Schimek <mschimek@users.sourceforge.net>
2008-03-01 15:50:55 +0000

    Copied from branch 0.2. Resynched with branch 0.2 line feed cosmetics.
    git-svn-id: https://svn.code.sf.net/p/zapping/svn/branches/branch-0-3/vbi@4023 746cafe9-179e-40ba-aef4-f754c8c453d8

--- test/test-dvb_demux.cc
+++ test/test-dvb_demux.cc
@@ -15,10 +15,11 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
  */
 
-/* $Id: test-dvb_demux.cc,v 1.2.2.2 2008-02-25 20:57:16 mschimek Exp $ */
+/* $Id: test-dvb_demux.cc,v 1.2.2.3 2008-03-01 15:50:55 mschimek Exp $ */
 
 #undef NDEBUG
 
