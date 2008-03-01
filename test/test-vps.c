b98c572f0b015851845505bd4779983a847ad6c2
Michael H. Schimek <mschimek@users.sourceforge.net>
2008-03-01 15:51:45 +0000

    Resynched with branch 0.2 line feed cosmetics, include config.h.
    git-svn-id: https://svn.code.sf.net/p/zapping/svn/branches/branch-0-3/vbi@4029 746cafe9-179e-40ba-aef4-f754c8c453d8

--- test/test-vps.c
+++ test/test-vps.c
@@ -15,12 +15,17 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
  */
 
-/* $Id: test-vps.c,v 1.1.2.3 2007-11-01 00:21:26 mschimek Exp $ */
+/* $Id: test-vps.c,v 1.1.2.4 2008-03-01 15:51:45 mschimek Exp $ */
 
 #undef NDEBUG
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
 
 #include <assert.h>
 #include <stdlib.h>
