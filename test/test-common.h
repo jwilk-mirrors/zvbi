c317b01e4806d17d2cb3afed5f846c22af15e3a6
Michael H. Schimek <mschimek@users.sourceforge.net>
2008-03-01 15:50:59 +0000

    Rewrote the GCC __attribute__ wrapper macros for compatibility with strict ISO C. Resynched with branch 0.2 line feed cosmetics.
    git-svn-id: https://svn.code.sf.net/p/zapping/svn/branches/branch-0-3/vbi@4024 746cafe9-179e-40ba-aef4-f754c8c453d8

--- test/test-common.h
+++ test/test-common.h
@@ -15,10 +15,11 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA 02110-1301, USA.
  */
 
-/* $Id: test-common.h,v 1.2.2.3 2008-02-25 20:58:51 mschimek Exp $ */
+/* $Id: test-common.h,v 1.2.2.4 2008-03-01 15:50:59 mschimek Exp $ */
 
 #include <string.h>
 #include <inttypes.h>
