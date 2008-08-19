2970f6bcdf5f7c1e876bd6dcdb74a5591b4c818a
Michael H. Schimek <mschimek@users.sourceforge.net>
2008-08-19 13:14:30 +0000

    Moved the C++ interface here from src/bcd.h.
    git-svn-id: https://svn.code.sf.net/p/zapping/svn/branches/branch-0-3/vbi@4075 746cafe9-179e-40ba-aef4-f754c8c453d8

--- test/test-bcd.cc
+++ test/test-bcd.cc
@@ -19,20 +19,94 @@
  *  MA 02110-1301, USA.
  */
 
-/* $Id: test-bcd.cc,v 1.1.2.8 2008-03-01 15:51:48 mschimek Exp $ */
+/* $Id: test-bcd.cc,v 1.1.2.9 2008-08-19 13:14:30 mschimek Exp $ */
 
 #undef NDEBUG
 #include <assert.h>
-#include "src/zvbi.h"
+
+#include "zvbi/bcd.h"
+
+#include <iostream>
+#include <iomanip>
+
+namespace vbi {
+  class Bcd {
+    int	value;
+
+  public:
+    Bcd () {};
+    Bcd (int n) : value (n) {};
+
+    operator int () const
+      { return value; };
+
+    int bin (void)
+      { return vbi_bcd2bin (value); };
+    Bcd& bcd (int n)
+      { value = vbi_bin2bcd (n); return *this; };
+
+    Bcd operator + ()
+      { return *this; };
+    Bcd operator - ()
+      { Bcd t (vbi_neg_bcd (value)); return t; };
+
+    Bcd& operator += (const Bcd& other)
+      { value = vbi_add_bcd (value, other.value); return *this; };
+    Bcd& operator ++ ()
+      { value = vbi_add_bcd (value, 0x1); return *this; };
+    Bcd operator ++ (int)
+      { Bcd t = *this; ++*this; return t; };
+
+    Bcd& operator -= (const Bcd& other)
+      { value = vbi_sub_bcd (value, other.value); return *this; };
+    Bcd& operator -- ()
+      { value = vbi_sub_bcd (value, 0x1); return *this; };
+    Bcd operator -- (int)
+      { Bcd t = *this; --*this; return t; };
+
+    bool operator == (int n) const
+      { return value == n; };
+    bool operator == (const Bcd& other) const
+      { return value == other.value; };
+    bool operator != (int n) const
+      { return value != n; };
+    bool operator != (const Bcd& other) const
+      { return value != other.value; };
+
+    bool valid () const
+      { return vbi_is_bcd (value); };
+  };
+
+  static inline Bcd operator +	(Bcd a, const Bcd& b)
+    { a += b; return a; }
+  static inline Bcd operator -	(Bcd a, const Bcd& b)
+    { a -= b; return a; }
+  static inline
+    std::ostream& operator <<	(std::ostream&		stream,
+				 const Bcd&		b)
+    {
+      std::ios::fmtflags flags = stream.flags ();
+
+      stream << std::hex << (int) b;
+      stream.flags (flags);
+      return stream;
+    }
+
+  typedef Bcd Pgno;
+  typedef Bcd Subno;
+
+  static const Bcd any_subno (VBI_ANY_SUBNO);
+  static const Bcd no_subno (VBI_NO_SUBNO);
+}
 
 #define INT_BITS (sizeof (int) * 8)
 
 static int
 modulo				(int			n)
 {
-	static const int m = -VBI3_BCD_BIN_MIN;
+	static const int m = -VBI_BCD_BIN_MIN;
 
-	if (n == VBI3_BCD_BIN_MIN)
+	if (n == VBI_BCD_BIN_MIN)
 		return n;
 	else if (n < 0)
 		return -((-n) % m);
@@ -51,7 +125,7 @@ long_random			()
 	return r;
 }
 
-static vbi3_bool
+static vbi_bool
 digits_greater			(unsigned int		a,
 				 unsigned int		b)
 {
@@ -76,19 +150,19 @@ main				(void)
 	assert (0x3F7F == vbi::any_subno);
 	assert (0x3F7F == vbi::no_subno);
 
-	assert (VBI3_BCD_BIN_MIN == vbi3_bcd2bin (VBI3_BCD_MIN));
-	assert (VBI3_BCD_BIN_MAX == vbi3_bcd2bin (VBI3_BCD_MAX));
+	assert (VBI_BCD_BIN_MIN == vbi_bcd2bin (VBI_BCD_MIN));
+	assert (VBI_BCD_BIN_MAX == vbi_bcd2bin (VBI_BCD_MAX));
 
 	for (i = -70000; i < 110000; ++i) {
 		switch (i / 10000) {
 		case 7:
-			x = VBI3_BCD_BIN_MAX;
+			x = VBI_BCD_BIN_MAX;
 			break;
 		case 8:
 			x = 0;
 			break;
 		case 9:
-			x = VBI3_BCD_BIN_MIN;
+			x = VBI_BCD_BIN_MIN;
 			break;
 		case 10:
 			x = modulo (long_random ());
@@ -98,8 +172,8 @@ main				(void)
 			break;
 		}
 
-		y = vbi3_bin2bcd (x);
-		z = vbi3_bcd2bin (y);
+		y = vbi_bin2bcd (x);
+		z = vbi_bcd2bin (y);
 		assert (x == z);
 
 		a.bcd (x);
@@ -120,13 +194,13 @@ main				(void)
 
 		switch (i & 0xF00) {
 		case 0x200:
-			y = VBI3_BCD_BIN_MAX;
+			y = VBI_BCD_BIN_MAX;
 			break;
 		case 0x700:
 			y = 0;
 			break;
 		case 0xB00:
-			y = VBI3_BCD_BIN_MIN;
+			y = VBI_BCD_BIN_MIN;
 			break;
 		default:
 			y = modulo (long_random ());
@@ -174,13 +248,13 @@ main				(void)
 			b.bcd (y);
 
 			assert (digits_greater (d & (int) b, (int) a)
-				== vbi3_bcd_digits_greater
+				== vbi_bcd_digits_greater
 				(d & (int) b, (int) a));
 
 			y = d & random();
 
 			assert (digits_greater (y, (int) a)
-				== vbi3_bcd_digits_greater (y, (int) a));
+				== vbi_bcd_digits_greater (y, (int) a));
 		}
 	}
 
