403dc3846bcc7d5d2ac8c3aab8224f093089673a
Michael H. Schimek <mschimek@users.sourceforge.net>
2008-08-20 12:35:04 +0000

    Moved here from src/, cleaned up, added allocation functions.
    git-svn-id: https://svn.code.sf.net/p/zapping/svn/branches/branch-0-3/vbi@4099 746cafe9-179e-40ba-aef4-f754c8c453d8

---
+++ zvbi/link.c
@@ -0,0 +1,289 @@
+/*
+ *  libzvbi -- Links
+ *
+ *  Copyright (C) 2001-2008 Michael H. Schimek
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id: link.c,v 1.1.2.1 2008-08-20 12:35:00 mschimek Exp $ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include "misc.h"
+#include "link.h"
+
+/**
+ */
+const char *
+_vbi_link_type_name		(vbi_link_type		type)
+{
+#undef CASE
+#define CASE(type) case VBI_LINK_##type : return #type ;
+
+	switch (type) {
+	CASE (NONE)
+	CASE (MESSAGE)
+	CASE (TTX_PAGE)
+	CASE (HTTP)
+	CASE (FTP)
+	CASE (EMAIL)
+	}
+
+#undef CASE
+
+	errno = VBI_ERR_INVALID_LINK_TYPE;
+
+	return NULL;
+}
+
+/** @internal */
+vbi_bool
+_vbi_link_dump			(const vbi_link *	lk,
+				 FILE *			fp)
+{
+	const char *type_name;
+	struct tm tm;
+
+	_vbi_null_check (lk, FALSE);
+	_vbi_null_check (fp, FALSE);
+
+	type_name = _vbi_link_type_name (lk->type);
+	if (NULL == type_name)
+		return FALSE;
+
+	if (NULL != lk->name) {
+		fprintf (fp, "%s name='%s' ",
+			 type_name, lk->name);
+	} else {
+		fprintf (fp, "%s name=NULL ",
+			 type_name);
+	}
+
+	switch (lk->type) {
+	case VBI_LINK_TTX_PAGE:
+		if (NULL != lk->lk.ttx.network) {
+			fputs ("network={", fp);
+			_vbi_network_dump (lk->lk.ttx.network, fp); 
+			fprintf (fp, "} pgno=%X subno=%X ",
+				 lk->lk.ttx.pgno,
+				 lk->lk.ttx.subno);
+		} else {
+			fprintf (fp, "network=NULL pgno=%X subno=%X ",
+				 lk->lk.ttx.pgno,
+				 lk->lk.ttx.subno);
+		}
+		break;
+
+	case VBI_LINK_HTTP:
+	case VBI_LINK_FTP:
+	case VBI_LINK_EMAIL:
+		if (NULL != lk->lk.web.url)
+			fprintf (fp, "url='%s' ", lk->lk.web.url);
+		else
+			fputs ("url=NULL ", fp);
+		if (NULL != lk->lk.web.script)
+			fprintf (fp, "script='%s' ", lk->lk.web.script);
+		else
+			fputs ("script=NULL ", fp);
+		break;
+
+	default:
+		break;
+	}
+
+	if (NULL != gmtime_r (&lk->expiration_time, &tm)) {
+		fprintf (fp, "expires=%04u-%02u-%02uT%02u:%02u:%02uZ",
+			 tm.tm_year + 1900,
+			 tm.tm_mon + 1,
+			 tm.tm_mday,
+			 tm.tm_hour,
+			 tm.tm_min,
+			 tm.tm_sec);
+	} else {
+		fputs ("expires=INVALID", fp);
+	}
+
+	return !ferror (fp);
+}
+
+/**
+ */
+vbi_bool
+vbi_link_destroy		(vbi_link *		lk)
+{
+	_vbi_null_check (lk, FALSE);
+
+	vbi_free (lk->name);
+
+	switch (lk->type) {
+	case VBI_LINK_TTX_PAGE:
+		vbi_network_delete (lk->lk.ttx.network);
+		break;
+
+	case VBI_LINK_HTTP:
+	case VBI_LINK_FTP:
+	case VBI_LINK_EMAIL:
+		vbi_free (lk->lk.web.url);
+		vbi_free (lk->lk.web.script);
+		break;
+
+	default:
+		errno = VBI_ERR_INVALID_LINK_TYPE;
+		return FALSE;
+	}
+
+	CLEAR (*lk);
+
+	return TRUE;
+}
+
+/**
+ */
+vbi_bool
+vbi_link_copy			(vbi_link *		dst,
+				 const vbi_link *	src)
+{
+	vbi_link new_link;
+	int saved_errno;
+
+	_vbi_null_check (dst, FALSE);
+
+	CLEAR (new_link);
+
+	if (NULL == src)
+		goto finish;
+
+	new_link.type = src->type;
+
+	switch (src->type) {
+	case VBI_LINK_NONE:
+		goto finish;
+
+	case VBI_LINK_MESSAGE:
+		break;
+
+	case VBI_LINK_TTX_PAGE:
+		if (NULL != src->lk.ttx.network) {
+			new_link.lk.ttx.network =
+				vbi_network_dup (src->lk.ttx.network);
+			if (unlikely (NULL == new_link.lk.ttx.network))
+				goto failed;
+		}
+		new_link.lk.ttx.pgno = src->lk.ttx.pgno;
+		new_link.lk.ttx.subno = src->lk.ttx.subno;
+		break;
+
+	case VBI_LINK_HTTP:
+	case VBI_LINK_FTP:
+	case VBI_LINK_EMAIL:
+		if (NULL != src->lk.web.url) {
+			new_link.lk.web.url = strdup (src->lk.web.url);
+			if (unlikely (NULL == new_link.lk.web.url))
+				goto failed;
+		}
+		if (VBI_LINK_EMAIL != src->type
+		    && NULL != src->lk.web.script) {
+			new_link.lk.web.script =
+				strdup (src->lk.web.script);
+			if (unlikely (NULL == new_link.lk.web.script))
+				goto failed;
+		}
+		break;
+
+	default:
+		errno = VBI_ERR_INVALID_LINK_TYPE;
+		return FALSE;
+	}
+
+	if (NULL != src->name) {
+		new_link.name = strdup (src->name);
+		if (unlikely (NULL == new_link.name))
+			goto failed;
+	}
+
+	new_link.expiration_time = src->expiration_time;
+
+ finish:
+	*dst = new_link;
+
+	return TRUE;
+
+ failed:
+	saved_errno = errno;
+	vbi_link_destroy (&new_link);
+	errno = saved_errno;
+
+	return FALSE;
+}
+
+/**
+ */
+vbi_bool
+vbi_link_init		(vbi_link *		lk)
+{
+	return vbi_link_copy (lk, NULL);
+}
+
+/**
+ */
+vbi_link *
+vbi_link_dup		(const vbi_link *	lk)
+{
+	vbi_link *new_lk;
+
+	new_lk = vbi_malloc (sizeof (*lk));
+	if (unlikely (NULL == new_lk))
+		return NULL;
+
+	if (unlikely (!vbi_link_copy (new_lk, lk))) {
+		int saved_errno = errno;
+
+		free (new_lk);
+		new_lk = NULL;
+
+		errno = saved_errno;
+	}
+
+	return new_lk;
+}
+
+/**
+ */
+void
+vbi_link_delete		(vbi_link *		lk)
+{
+	if (NULL != lk) {
+		vbi_link_destroy (lk);
+		free (lk);
+	}
+}
+
+/**
+ */
+vbi_link *
+vbi_link_new			(void)
+{
+	return vbi_link_dup (NULL);
+}
+
+/*
+Local variables:
+c-set-style: K&R
+c-basic-offset: 8
+End:
+*/
---
+++ zvbi/link.h
@@ -0,0 +1,189 @@
+/*
+ *  libzvbi -- Links
+ *
+ *  Copyright (C) 2000-2008 Michael H. Schimek
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id: link.h,v 1.1.2.1 2008-08-20 12:35:04 mschimek Exp $ */
+
+#ifndef __ZVBI_LINK_H__
+#define __ZVBI_LINK_H__
+
+#include <stdio.h>		/* FILE */
+#include <time.h>		/* time_t */
+
+#include "zvbi/network.h"	/* vbi_network */
+#include "zvbi/bcd.h"		/* vbi_pgno, vbi_subno */
+
+VBI_BEGIN_DECLS
+
+/**
+ * @brief Link type.
+ */
+typedef enum {
+	VBI_LINK_NONE = 0,
+
+	/**
+	 * This is just a message for display to the user. The @a
+	 * vbi_link->name field points to the message text.
+	 */
+	VBI_LINK_MESSAGE,
+
+	/**
+	 * A link to a Teletext page. @a vbi_link->lk.ttx.pgno and
+	 * lk.ttx.subno will be set. The @a lk.ttx.network field may
+	 * identify the network transmitting the page, which may not
+	 * be the network transmitting the link. The @a name field may
+	 * point to a description of the page for display to the user.
+	 */
+	VBI_LINK_TTX_PAGE,
+
+	/**
+	 * A link to a webpage. @a vbi_link->lk.web.url is a HTTP URL
+	 * like "http://zapping.sf.net". The @a name field may point
+	 * to a description of the page for display to the user.
+	 */
+	VBI_LINK_HTTP,
+
+	/**
+	 * @a vbi_link->lk.web.url is a FTP URL like
+	 * "ftp://foo.bar.com/baz". The @a name field may point to a
+	 * descriptive text for display to the user.
+	 */
+	VBI_LINK_FTP,
+
+	/**
+	 * @a vbi_link->lk.web.url is an e-mail address like
+	 * "mailto:foo@bar". The @a name field may point to a
+	 * descriptive text for display to the user.
+	 */
+	VBI_LINK_EMAIL
+} vbi_link_type;
+
+extern const char *
+_vbi_link_type_name		(vbi_link_type		type)
+  _vbi_const;
+
+/**
+ * @ingroup Event
+ *
+ * This structure describes a link transmitted by the network.
+ */
+typedef struct {
+	/** See vbi_link_type. */
+	vbi_link_type			type;
+
+	int				reserved1;
+
+	/**
+	 * Some descriptive text, a NUL-terminated string in locale
+	 * encoding. Can be @c NULL if no description is available.
+	 */
+	char *				name;
+
+	union {
+		struct {
+			/**
+			 * If this is a @c VBI_LINK_TTX_PAGE, this
+			 * field identifies the network transmitting
+			 * the page. Can be @c NULL if the network is
+			 * unknown.
+			 */
+			vbi_network *			network;
+
+			/** A Teletext page number. */
+			vbi_pgno			pgno;
+
+			/**
+			 * A Teletext subpage number. Can be @a
+			 * VBI_ANY_SUBNO.
+			 */
+			vbi_subno			subno;
+		}				ttx;
+
+		struct {
+			/**
+			 * A Uniform Resource Locator representing a
+			 * resource available on the Internet. This is
+			 * a NUL-terminated string in locale
+			 * encoding.
+			 */
+			char *				url;
+
+			/**
+			 * A piece of ECMAscript (ECMA-262) which may
+			 * trigger some action on WebTV or
+			 * SuperTeletext pages. This is a
+			 * NUL-terminated string in locale encoding.
+			 * Can be @c NULL if no script was
+			 * transmitted.
+			 */
+			char *				script;
+
+			int				reserved2;
+		}				web;
+
+		void *				reserved3[4];
+	}				lk;
+
+	/**
+	 * The time in seconds since 1970-01-01 00:00 UTC after which
+	 * the link should no longer be offered to the user.
+	 */
+	time_t				expiration_time;
+
+	void *				reserved4[2];
+	int				reserved5[2];
+} vbi_link;
+
+extern vbi_bool
+vbi_link_copy			(vbi_link *		dst,
+				 const vbi_link *	src)
+  _vbi_nonnull ((1));
+extern vbi_bool
+vbi_link_destroy		(vbi_link *		lk)
+  _vbi_nonnull ((1));
+extern vbi_bool
+vbi_link_init			(vbi_link *		lk)
+  _vbi_nonnull ((1));
+extern vbi_link *
+vbi_link_dup			(const vbi_link *	lk)
+  _vbi_alloc;
+extern void
+vbi_link_delete			(vbi_link *		lk)
+  _vbi_nonnull ((1));
+extern vbi_link *
+vbi_link_new			(void)
+  _vbi_alloc;
+
+/* Private */
+
+extern vbi_bool
+_vbi_link_dump			(const vbi_link *	lk,
+				 FILE *			fp)
+  _vbi_nonnull ((1, 2));
+
+VBI_END_DECLS
+
+#endif /* __ZVBI_LINK_H__ */
+
+/*
+Local variables:
+c-set-style: K&R
+c-basic-offset: 8
+End:
+*/
